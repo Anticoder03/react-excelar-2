072bbcbb7bc89265d05f3f095e6bc7be803e7a41
Anticoder03 <ap5381545@gmail.com>
2026-05-04 19:09:35 +0530

    update

--- src/components/Button2.jsx
+++ src/components/Button2.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
-const Button2 = ({text, icon}) => {
+const Button2 = (props) => {
+    // Proptype
+    // Button2.propTypes = {
+    //     text: React.PropTypes.string,
+    //     icon: React.PropTypes.element
+    // };
   return (
     <>
       <button>
-        {icon}
-        {text}
+        {props.icon}
+        {props.text}
       </button>
     </>
   )
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import { IoMdLogIn } from "react-icons/io";
 import { IoMdLogOut } from "react-icons/io";
 import Button2 from './Button2';
+import Container from './Container';
 const LandingPage = () => {
   return (
     <div>
       <Button2 text="Login" icon={<IoMdLogIn />} />
       <Button2 text="Logout" icon={<IoMdLogOut />} />
+      <Container>
+        <button>Button inside Container</button>
+        
+      </Container>
     </div>
   )
 }
